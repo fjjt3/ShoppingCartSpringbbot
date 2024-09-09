c5a40f5b6612272aa24eb193433312b452268d58
Francisco Javier Jimenez <>
2024-09-09 11:47:35 +0200

    first commit

---
+++ src/main/java/com/dailycodework/dreamshops/model/Product.java
@@ -0,0 +1,16 @@
+package com.dailycodework.dreamshops.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+    private Category category;
+    private List<Image> images;
+
+}
